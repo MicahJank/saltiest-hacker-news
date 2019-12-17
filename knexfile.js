9ddf627fad0d9a4283cb5a81573352901d290508
MicahJank <MicahJank@users.noreply.github.com>
2019-12-16 23:05:25 -0600

    set up knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/templateDB.db3' // <-- Replace
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { 
+      directory: './data/seeds'
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+
+};
